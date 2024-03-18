99ec6a703ffb74def3246491e03d37711a466fae
flub78 <frederic.peignot@free.fr>
2024-03-18 12:42:24 +0100

    really stable this time (I hope)

--- tests/Unit/BoardModelTest.php
+++ tests/Unit/BoardModelTest.php
@@ -8,6 +8,7 @@
 namespace Tests\Unit;
 
 use App\Models\Board;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 /**
@@ -15,6 +16,9 @@ use Tests\TestCase;
  */
 class BoardModelTest extends TestCase
 {
+
+    protected $log = true;
+
     /**
      * Test element creation, read, update and delete
      * Given the database server is on
@@ -24,15 +28,23 @@ class BoardModelTest extends TestCase
      */
     public function testCRUD(): void
     {
-        $this->assertTrue(true);
+        if ($this->log) {
+            Log::info("BoardModelTest.testCRUD");
+        }
         $initial_count = Board::count();
+        echo "initial_count: $initial_count\n";
+        if ($this->log) {
+            Log::info("BoardModelTest.testCRUD initial_count: $initial_count");
+        }
 
         // Create some elements
         $elt1 = Board::factory()->make();
+        $elt1_key = $elt1->name;
         $this->assertNotNull($elt1, "the element 1 has been created");
         $this->assertTrue($elt1->save(), "the element 1 has been saved in database");
 
         $elt2 = Board::factory()->make();
+        $elt2_key = $elt2->name;
         $this->assertNotNull($elt2, "the element 2 has been created");
         $this->assertTrue($elt2->save(), "the element 2 has been saved in database");
 
@@ -40,46 +52,58 @@ class BoardModelTest extends TestCase
         $elt3_key = $elt3->name;
         $this->assertNotNull($elt3, "the element 3 has been created");
         $this->assertTrue($elt3->save(), "the element 3 has been saved in database");
-        if ('name' == 'id') $elt3_key = $elt3->id;
-
+        if ('name' == 'id') {
+            $elt3_key = $elt3->id;
+        }
 
         $new_count = Board::count();
         $this->assertTrue($new_count == $initial_count + 3, "3 elements added to the database");
 
         // Read back the elements
-        $latest = Board::find($elt3_key);
-        $this->assertNotNull($latest, "an element can be fetched by its key: " . $elt3_key);
+        $relt1 = Board::find($elt1_key);
+        $this->assertNotNull($relt1, "element 1 can be fetched by its key: " . $elt1_key);
+
+        $relt2 = Board::find($elt2_key);
+        $this->assertNotNull($relt2, "element 2 can be fetched by its key: " . $elt2_key);
+
+        $relt3 = Board::find($elt3_key);
+        $this->assertNotNull($relt3, "element 2 can be fetched by its key: " . $elt3_key);
 
         // for csv_high_variability_fields
         // fields with low variability cannot be compared as they could be identical between two elements
         $diff = 0;
         $high_variability_fields = ["description", "href", "image", "lists"];
         foreach ($high_variability_fields as $key) {
-            if ($elt2->$key != $latest->$key) {
+            if ($relt2->$key != $relt3->$key) {
                 $diff++;
-                $latest->$key = $elt2->$key;
+                $relt3->$key = $relt2->$key;
             }
         }
 
-        $this->assertTrue($diff > 0, "at least 1 differences between elt2 and latest");
+        $this->assertTrue($diff > 0, "at least 1 differences between elt2 and relt3");
 
         // Update the element
         if ($diff > 0) {
-            $this->assertTrue($latest->save(), "updated element has been saved");
+            $this->assertTrue($relt3->save(), "updated element has been saved");
 
             // Read back the element
             $found = Board::find($elt3_key);
             $this->assertNotNull($found, "and it can be read back from database");
 
             foreach ($high_variability_fields as $key) {
-                $this->assertEquals($elt2->$key, $found->$key, "$key updated");
+                $this->assertEquals($relt2->$key, $found->$key, "$key updated");
             }
         }
 
+        echo "count before delete: " . Board::count() . "\n";
         // Delete the elements
-        $this->assertTrue($elt3->delete(), "the element 3 has been deleted from database");
-        $this->assertTrue($elt1->delete(), "the element 1 has been deleted from database");
-        $this->assertTrue($elt2->delete(), "the element 2 has been deleted from database");
+        $this->assertTrue($relt3->delete(), "the element 3 has been deleted from database");
+        echo "count after delete 3: " . Board::count() . "\n";
+        $this->assertTrue($relt1->delete(), "the element 1 has been deleted from database");
+        echo "count after delete 1: " . Board::count() . "\n";
+        $this->assertTrue($relt2->delete(), "the element 2 has been deleted from database");
+
+        echo "count after delete: " . Board::count() . "\n";
 
         $final_count = Board::count();
         $this->assertEquals($initial_count, $final_count, "\$initial_count:$initial_count == \$final_count: $final_count");
