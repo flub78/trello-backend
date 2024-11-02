d90614ff404f66998e455c006ade45baa6095816
flub78 <frederic.peignot>
2024-11-02 12:36:35 +0100

    WIP

--- setenv.py
+++ setenv.py
@@ -3,3 +3,10 @@
 
 # Databases to check
 databases = ["mysql", "information_schema", "nonexistent_db", "multi"]
+user = "root"
+password = ""
+# URLs to check
+urls = [
+    "http://localhost/index.php",
+    "http://localhost/phpmyadmin"
+]
